d9b780996a7b2809c33d73ae98fc5dc6101a4135
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-01-18 15:40:27 -0500

    EmployeeServiceImpl created for createEmployee

---
+++ ems-backend/src/main/java/com/employee/ems_backend/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.employee.ems_backend.service;
+
+import com.employee.ems_backend.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
