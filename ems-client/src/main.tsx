005e693a9c5185b26a2e9a9014d2c6495de0ae00
Bishal Paudel <bishalpaudel1998@gmail.com>
2025-02-18 21:50:59 -0500

    React Router setup

--- ems-client/src/main.tsx
+++ ems-client/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import ListEmployee from "./components/ListEmployee.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ListEmployee />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListEmployee />} />
+        <Route path="/employees" element={<ListEmployee />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
